024e6d5537b8defe26f3291dbf285b07cda7a07b
Yekeen Toyyib <yekeentoyyib@gmail.com>
2023-06-27 12:43:42 +0100

    created new files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom"
+import About from './files/About';
+import Contact from './files/Contact';
+import Error from './files/Error';
+import Index from './files/Index';
 
 const App = () => {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/*" element={<Error />}/>
+      </Routes>
     </>
   )
 }
