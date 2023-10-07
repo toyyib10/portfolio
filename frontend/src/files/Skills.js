9d67c6fd7b176c51f87ee6871b9637a08bfc2b9d
TOYYIB YEKEEN <yekeentoyyib@gmail.com>
2023-10-07 14:42:59 +0100

    worked on skills

--- frontend/src/files/Skills.js
+++ frontend/src/files/Skills.js
@@ -7,50 +7,65 @@ const Skills = () => {
       <section className='section'>
         <Header/>
         <div className='d-flex px-5 pt-2 flex-wrap align-items-center justify-content-evenly'>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>HTML</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white  m-0" style={{"color":"grey","fontSize":"bold"}}>HTML</p>
+            <img height="85px" className="bg-white" src='images/HTML.png' alt='HTML'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>CSS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white  m-0" style={{"color":"grey","fontSize":"bold"}}>CSS</p>
+            <img height="85px" className="bg-white" src='images/CSS.png' alt='CSS'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>BOOTSTRAP</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>BOOTSTRAP</p>
+            <img height="85px" className="bg-white" src='images/BOOTSTRAP.png' alt='BOOTSTRAP'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>GIT</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>GIT</p>
+            <img height="85px" className="bg-white" src='images/GIT.png' alt='GIT'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>JAVASCRIPT</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>JAVASCRIPT</p>
+            <img height="85px" className="bg-white" src='images/JAVASCRIPT.png' alt='JAVASCRIPT'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>REACT JS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>REACT JS</p>
+            <img height="85px" className="bg-white" src='images/REACT.png' alt='REACT'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>NODE JS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>NODE JS</p>
+            <img height="85px" className="bg-white" src='images/NODE.png' alt='NODE'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>PYTHON</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>PYTHON</p>
+            <img height="85px" className="bg-white" src='images/PYTHON.png' alt='PYTHON'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>CHART JS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>CHART JS</p>
+            <img height="85px" className="bg-white" src='images/CHART.png' alt='CHART'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>EXPRESS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>EXPRESS</p>
+            <img height="85px" className="bg-white" src='images/EXPRESS.png' alt='EXPRESS'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>ARDUINO</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>ARDUINO</p>
+            <img height="85px" className="bg-white" src='images/ARDUINO.png' alt='ARDUINO'/>
           </div>  
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>REDUX</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>REDUX</p>
+            <img height="85px" className="bg-white" src='images/REDUX.png' alt='REDUX'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>MONGO DB</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>MONGO DB</p>
+            <img height="85px" className="bg-white" src='images/MONGO.png' alt='MONGO'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>AOS</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>AOS</p>
+            <img height="85px" className="bg-white" src='images/AOS.png' alt='AOS'/>
           </div>
-          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white' style={{"height":"130px"}}>
-            <p style={{"color":"grey","fontSize":"bold"}}>HOSTING</p>
+          <div className='shadow col-md-2 p-2 col-4 m-2 m-md-3 bg-white d-flex flex-column align-items-center' style={{"height":"130px"}}>
+            <p className="bg-white m-0" style={{"color":"grey","fontSize":"bold"}}>HOSTING</p>
+            <img height="85px" className="bg-white" src='images/HOSTING.png' alt='HOSTING'/>
           </div>
         </div>
       </section>
